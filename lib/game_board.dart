b9ee95d6ebc0131783220fa8e651d7dcd810e90a
Gowtham MG <gowthamgandhi123@gmail.com>
2020-07-06 07:31:18 +0530

    board initialization logic completed

--- lib/game_board.dart
+++ lib/game_board.dart
@@ -37,6 +37,121 @@ class GameBoard extends StatelessWidget {
 
   int squaresLeft;
 
+  void initialiseGame(){
+    boardSquares = List.generate(rowCount, (index) => List.generate(columnCount, (index) => BoardSquare()));
+
+    openedSquares = List.generate(rowCount * columnCount, (i) => false);
+
+    flaggedSquares = List.generate(rowCount * columnCount, (i) => false);
+
+    bombCount = 0;
+    squaresLeft = rowCount * columnCount;
+
+    Random random = new Random();
+    for (int i = 0; i < rowCount; i++) {
+      for (int j = 0; j < columnCount; j++) {
+        int randomNumber = random.nextInt(maxSquareLimit);
+        if (randomNumber < bombProbabilityLimit) {
+          boardSquares[i][j].hasBomb = true;
+          bombCount++;
+        }
+      }
+    }
+    for (int i = 0; i < rowCount; i++) {
+      for (int j = 0; j < columnCount; j++) {
+        if (i > 0 && j > 0) {
+          if (boardSquares[i - 1][j - 1].hasBomb) {
+            boardSquares[i][j].bombsAround++;
+          }
+        }
+
+        if (i > 0) {
+          if (boardSquares[i - 1][j].hasBomb) {
+            boardSquares[i][j].bombsAround++;
+          }
+        }
+
+        if (i > 0 && j < columnCount - 1) {
+          if (boardSquares[i - 1][j + 1].hasBomb) {
+            boardSquares[i][j].bombsAround++;
+          }
+        }
+
+        if (j > 0) {
+          if (boardSquares[i][j - 1].hasBomb) {
+            boardSquares[i][j].bombsAround++;
+          }
+        }
+
+        if (j < columnCount - 1) {
+          if (boardSquares[i][j + 1].hasBomb) {
+            boardSquares[i][j].bombsAround++;
+          }
+        }
+
+        if (i < rowCount - 1 && j > 0) {
+          if (boardSquares[i + 1][j - 1].hasBomb) {
+            boardSquares[i][j].bombsAround++;
+          }
+        }
+
+        if (i < rowCount - 1) {
+          if (boardSquares[i + 1][j].hasBomb) {
+            boardSquares[i][j].bombsAround++;
+          }
+        }
+
+        if (i < rowCount - 1 && j < columnCount - 1) {
+          if (boardSquares[i + 1][j + 1].hasBomb) {
+            boardSquares[i][j].bombsAround++;
+          }
+        }
+      }
+    }
+  }
+
+  void _handleTap(int i, int j) {
+
+    int position = (i * columnCount) + j;
+    openedSquares[position] = true;
+    squaresLeft = squaresLeft - 1;
+
+    if (i > 0) {
+      if (!boardSquares[i - 1][j].hasBomb &&
+          openedSquares[((i - 1) * columnCount) + j] != true) {
+        if (boardSquares[i][j].bombsAround == 0) {
+          _handleTap(i - 1, j);
+        }
+      }
+    }
+
+    if (j > 0) {
+      if (!boardSquares[i][j - 1].hasBomb &&
+          openedSquares[(i * columnCount) + j - 1] != true) {
+        if (boardSquares[i][j].bombsAround == 0) {
+          _handleTap(i, j - 1);
+        }
+      }
+    }
+
+    if (j < columnCount - 1) {
+      if (!boardSquares[i][j + 1].hasBomb &&
+          openedSquares[(i * columnCount) + j + 1] != true) {
+        if (boardSquares[i][j].bombsAround == 0) {
+          _handleTap(i, j + 1);
+        }
+      }
+    }
+
+    if (i < rowCount - 1) {
+      if (!boardSquares[i + 1][j].hasBomb &&
+          openedSquares[((i + 1) * columnCount) + j] != true) {
+        if (boardSquares[i][j].bombsAround == 0) {
+          _handleTap(i + 1, j);
+        }
+      }
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Container(
